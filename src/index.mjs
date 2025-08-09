346c75ef674fb8a8f949f70e04d7f46658a31cbb
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2025-08-09 14:50:02 +0900

    No logs in test

--- src/index.mjs
+++ src/index.mjs
@@ -9,18 +9,20 @@ app.get("*", (c) => {
   const content = RESPONSE_CONTENT ?? "Hello World";
   const statusCode = RESPONSE_STATUS ? Number(RESPONSE_STATUS) : 200;
   const contentType = RESPONSE_CONTENT_TYPE ?? "text/plain";
-  console.log(
-    JSON.stringify({
-      level: "info",
-      message: `${c.req.method} ${c.req.url} - ${statusCode} ${contentType}`,
-      path: c.req.path,
-      method: c.req.method,
-      status: statusCode,
-      contentType,
-      content,
-      requestHeader: c.req.header(),
-    }),
-  );
+  if (process.env.NODE_ENV !== "test" && !process.env.VITEST) {
+    console.log(
+      JSON.stringify({
+        level: "info",
+        message: `${c.req.method} ${c.req.url} - ${statusCode} ${contentType}`,
+        path: c.req.path,
+        method: c.req.method,
+        status: statusCode,
+        contentType,
+        content,
+        requestHeader: c.req.header(),
+      }),
+    );
+  }
 
   return c.body(content, statusCode, {
     "Content-Type": contentType,
